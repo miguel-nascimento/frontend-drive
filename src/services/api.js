24edc96ebae72406dcbab77a07d4f605c0a874c7
Miguel Gomes <miguelgomes13@live.com>
2019-04-13 10:17:58 -0300

    Testando novas funcionalidades

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://backend-curso-omnistack.herokuapp.com"
+    baseURL: "https://omnistack-backend.herokuapp.com"
 });
 
 export default api;
